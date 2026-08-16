4c3900db8fdccca7dd344bfccb0043ae8686f3d4
Shaxzod <shaxzodsadullayev1519@gmail.com>
2026-08-16 23:25:30 +0500

    LeetCode array 2 issue

--- LeetCode/Array/main.py
+++ LeetCode/Array/main.py
@@ -1,26 +1,32 @@
-#Type Eas
-class Talaba:
-    def __init__(self,ism,familya,tyil):
-        self.ism=ism
-        self.familya=familya
-        self.tugilgan_yil=tyil
-        self.bosqich=1
-    def get_name(self):
-        return self.ism
-    def get_lastname(self):
-        return self.familya
-    def get_age(self):
-        return self.tugilgan_yil
-    def get_info(self):
-        return f"{self.ism} {self.bosqich} - bosqich talabasi"
-    def set_bosqich(self,yangi_bosqich):
-        self.bosqich=yangi_bosqich
-    def update_boshqich(self):
-        self.bosqich+=1
-talaba1=Talaba("Shaxzod","Sadullayev",2005)
-print(talaba1.get_info())
-talaba1.update_boshqich()
-talaba1.update_boshqich()
-talaba1.update_boshqich()
-print(talaba1.get_info())
-    
+# Type Easy
+#Senga nums nomli butun sonlardan tashkil topgan massiv va val nomli butun son beriladi. Vazifa — massiv ichidagi val ga teng bo‘lgan barcha elementlarni olib tashlash. Buni yangi massiv yaratmasdan, ya’ni massivning o‘zini o‘zgartirgan holda bajarish kerak. val ga teng bo‘lmagan barcha elementlar massivning boshiga joylashtirilishi kerak. Ularning soni k bo‘ladi va funksiya k qiymatini qaytarishi kerak. Massivning birinchi k ta elementi val ga teng bo‘lmagan elementlardan iborat bo‘lishi shart. k dan keyingi elementlarning qanday qiymatda bo‘lishi muhim emas. Elementlarning tartibini o‘zgartirish mumkin. Masalan, nums = [3,2,2,3] va val = 3 bo‘lsa, 3 lar olib tashlanadi, birinchi ikkita element [2,2] bo‘ladi va funksiya 2 ni qaytaradi.
+from typing import List
+def removeElement(self, nums: List[int], val: int) -> int:
+        index = 0
+
+        for num in nums:
+            if num != val:
+                nums[index] = num
+                index += 1
+
+        return index
+
+
+# Har bir element aksiyaning o‘sha kundagi narxini bildiradi. Bitta kunda sotib olib, undan keyingi boshqa bir kunda sotish kerak. Maqsad — sotib olish va sotish orasidagi eng katta foydani topish. Agar foyda qilishning iloji bo‘lmasa, 0 qaytariladi.
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = prices[0]
+        max_profit = 0
+
+        for price in prices:
+            if price < min_price:
+                min_price = price
+
+            profit = price - min_price
+
+            if profit > max_profit:
+                max_profit = profit
+
+        return max_profit
